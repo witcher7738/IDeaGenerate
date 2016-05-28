1d6bed8d346704cbe058a4b0f4843fae6808bb47
witcher7738 <witcher7738@gmail.com>
2016-05-28 12:51:22 +0300

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using WordGenerator;
 
 namespace WeatherApp
 {
@@ -12,19 +13,21 @@ namespace WeatherApp
     public class MainActivity : Activity
     {
         int count = 1;
-
+        WordGenerator Words;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
+            
+            var words = new List<string>(){ "word", "anotherword", "test", "testest"};
+            Words = new WordGenerator(words);
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button.Text = string.Format("{0}", Words.GenerateWord()); };
         }
     }
 }
